6df15575455c2bb7f11323516930440ff09fbea1
Hpsaturn <hpsaturn@gmail.com>
2022-03-15 10:01:32 +0100

    added location class and fields on response. Some minor fixes

--- canairioapi.py
+++ canairioapi.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from influxdb import InfluxDBClient
+import pygeohash as pgh
 from datetime import datetime
 import os
 
@@ -14,11 +15,17 @@ dbname = os.environ.get("CANAIRIO_INFLUX_DBNAME")
 client = InfluxDBClient(host=host, port=port, database=dbname)
 
 def getMeasure(field, ftype, fvalue, funit):
+  """ Get a measure from a field
+  :param field: complete field row data from influxdb
+  :param ftype: field type name
+  :param fvalue: field name on influxdb
+  :param funit: field unit name or symbol
+  """
   measure = {'measurementID': field['time']}
   measure['measurementType'] = ftype
   measure['measurementUnit'] = funit
   measure['measurementDeterminedDate '] = field['time'].format('YYYY-MM-DD HH:mm:ss')
-  measure['measurementDeterminedBy'] = 'CanAirIO'  # TODO: get from field
+  measure['measurementDeterminedBy'] = 'CanAirIO station {}'.format(field['name'])
   try:
     measure['measurementValue'] = field['{}'.format(fvalue)]
   except Exception as e:
@@ -28,13 +35,19 @@ def getMeasure(field, ftype, fvalue, funit):
 
 
 def addStation(station, sdata):
+  """ Add a station to API response
+  :param station: station name
+  :param sdata: all stations data
+  :return: station json data
+  """
   response = {'id': station}
   response['station_name'] = station
-  response['scientificName'] = 'Air Quality'
+  response['scientificName'] = 'Air quality fixed station'
   response['ownerInstitutionCodeProperty'] = 'CanAirIO'
+  response['type'] = 'PhysicalObject'
+  response['license'] = 'CC BY-NC-SA'
   data = []
-  ffirst = sdata[0]
-  fend = sdata[0]
+  fend = {}
   for s in sdata:
     if s['name'] == station:
       data.append(getMeasure(s, 'PM1', 'pm1', 'ug/m3'))
@@ -45,7 +58,7 @@ def addStation(station, sdata):
       data.append(getMeasure(s, 'Pressure', 'prs', 'hPa'))
       data.append(getMeasure(s, 'CO2', 'co2', 'ppm'))
       data.append(getMeasure(s, 'CO2 Temperature', 'co2tmp', 'C'))
-      data.append(getMeasure(s, 'CO2 Humidity', 'co2hum', 'C'))
+      data.append(getMeasure(s, 'CO2 Humidity', 'co2hum', '%'))
       data.append(getMeasure(s, 'Battery voltage', 'vbat', 'V'))
       data.append(getMeasure(s, 'Geohash', 'geo', ''))
       data.append(getMeasure(s, 'Geohash3', 'geo3', ''))
@@ -53,6 +66,14 @@ def addStation(station, sdata):
       fend = s
       break
   response['measurements'] = data
+  response['observedOn'] = fend['time'].format('YYYY-MM-DD HH:mm:ss')
+  coords = pgh.decode(fend['geo'])
+  response['locationID'] = fend['time']
+  response['decimalLatitude '] = coords[0]
+  response['decimalLongitude '] = coords[1]
+  response['georeferencedBy'] = 'CanAirIO firmware {}'.format(fend['rev'])
+  response['georeferencedDate'] = fend['time'].format('YYYY-MM-DD HH:mm:ss')
+  response['geohash'] = fend['geo']
   return response
 
 
